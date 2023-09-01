739385dc092674ae4d60fbc5fb2393bf91235f14
EJR Bisoj <bishojbk@gmail.com>
2023-09-01 11:17:51 +0545

    design almost

--- jadoo-website/main.js
+++ jadoo-website/main.js
@@ -1,1 +1,6 @@
+const input = document.querySelector(".email");
+const emailImage = document.querySelector(".img-email");
 
+input.addEventListener("click", () => {
+  emailImage.style.display = "none";
+});
