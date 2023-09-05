5d78fa3deaeba5de7226d9c14478b0ac5266df38
EJR Bisoj <bishojbk@gmail.com>
2023-09-05 18:03:28 +0545

    working on responsiveness

--- jadoo-website/main.js
+++ jadoo-website/main.js
@@ -6,9 +6,22 @@ const cardFirst = document.querySelector(".card1");
 const cardSecond = document.querySelector(".card2");
 const msgSuccess = document.querySelector(".msg-success");
 const subBtn = document.querySelector(".btn-sub");
-const imgHeart = document.querySelector(".img-heart");
-const dots1 = document.querySelector(".dot1");
-const dots2 = document.querySelector(".dot2");
+const hamMenuItems = document.querySelector(".ham-menu-items");
+const hamMenu = document.querySelector(".navbar-hamburger-menu");
+const bgBlack = document.querySelector(".background-black");
+const crossImg = document.querySelector(".img-cross");
+
+hamMenu.addEventListener("click", () => {
+  hamMenuItems.classList.toggle("show");
+  bgBlack.classList.toggle("show");
+  document.body.classList.toggle("no-scroll");
+});
+
+crossImg.addEventListener("click", () => {
+  hamMenuItems.classList.toggle("show");
+  bgBlack.classList.toggle("show");
+  document.body.classList.toggle("no-scroll");
+});
 
 function isValidEmail(email) {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -16,76 +29,29 @@ function isValidEmail(email) {
   return emailPattern.test(email);
 }
 
-imgHeart.addEventListener("click", () => {});
-
-downArrow.addEventListener("click", () => {
-  if (!downArrow.classList.contains("opacity")) {
-    Card();
-  }
-});
-
-dots1.addEventListener("click", () => {
-  if (!upArrow.classList.contains("opacity")) {
-    Card();
-  }
-});
-
-dots2.addEventListener("click", () => {
-  if (!downArrow.classList.contains("opacity")) {
-    Card();
-  }
-});
-
-upArrow.addEventListener("click", () => {
-  if (!upArrow.classList.contains("opacity")) {
-    Card();
-  }
-});
-
 subBtn.addEventListener("click", () => {
   const email = document.querySelector(".email").value;
   msgSuccess.style.visibility = "visible";
 
   if (isValidEmail(email)) {
+    msgSuccess.classList.add("success");
     msgSuccess.textContent = "Thank you for subscribing to us.";
   } else {
+    msgSuccess.classList.add("fail");
     msgSuccess.textContent = "Sorry! We couldnt find your email.";
   }
 
   setTimeout(function () {
     msgSuccess.style.visibility = "hidden";
-  }, 2000);
+  }, 3000);
 });
 
 input.addEventListener("click", () => {
   emailImage.style.display = "none";
 });
 
-function Card() {
-  const card1 = document.querySelector(".card1");
-  const card2 = document.querySelector(".card2");
-  const card1Styles = window.getComputedStyle(card1);
-  const card2Styles = window.getComputedStyle(card2);
-
-  const card2Left = card2Styles.getPropertyValue("left");
-  const card2Top = card2Styles.getPropertyValue("top");
-  const card2Z = card2Styles.getPropertyValue("z-index");
-
-  const card1Left = card1Styles.getPropertyValue("left");
-  const card1Top = card1Styles.getPropertyValue("top");
-  const card1Z = card1Styles.getPropertyValue("z-index");
-
-  card1.style.top = card2Top;
-  card1.style.left = card2Left;
-  card1.style.zIndex = card2Z;
-
-  card2.style.top = card1Top;
-  card2.style.left = card1Left;
-  card2.style.zIndex = card1Z;
-
-  dots1.classList.toggle("active");
-  dots2.classList.toggle("active");
-
-  upArrow.classList.toggle("opacity");
-  downArrow.classList.toggle("opacity");
-}
+$(document).ready(function () {
+  $(".testimonials-section-people").slick({
+    dots: true,
+  });
+});
