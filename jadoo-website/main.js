8256049f31823da1de83b5f9a9ae4f0db6a2cc79
EJR Bisoj <bishojbk@gmail.com>
2023-09-11 14:13:58 +0545

    almost final

--- jadoo-website/main.js
+++ jadoo-website/main.js
@@ -1,18 +1,22 @@
 const input = document.querySelector(".email");
 const emailImage = document.querySelector(".img-email");
-const msgSuccess = document.querySelector(".msg-success");
+
 const subBtn = document.querySelector(".btn-sub");
 const hamMenuItems = document.querySelector(".ham-menu-items");
 const hamMenu = document.querySelector(".navbar-hamburger-menu");
 const crossImg = document.querySelector(".img-cross");
 const hamMenuNav = document.querySelector(".mob-nav-show");
-const successTick = document.querySelector(".img-tick-blue");
-const failTick = document.querySelector(".img-tick-red");
+
 const header = document.querySelector(".header");
 const bgBlack = document.querySelector(".bg-black");
 const videoPlayer = document.querySelector(".video-section");
 const imgPlay = document.querySelector(".img-play");
 const crossBlack = document.querySelector(".black-cross");
+const scroll = document.getElementById("btn-back-to-top");
+const successMsg = document.querySelector(".success-msg");
+const failMsg = document.querySelector(".success-fail");
+const iframe = document.querySelector(".video");
+const play = document.querySelector(".btn-play");
 
 hamMenu.addEventListener("click", () => {
   document.body.classList.add("mobile-nav-show");
@@ -29,23 +33,22 @@ function isValidEmail(email) {
 }
 
 subBtn.addEventListener("click", () => {
-  msgSuccess.classList.remove("success");
-  msgSuccess.classList.remove("fail");
-  msgSuccess.style.visibility = "hidden";
-  successTick.style.display = "none";
-  failTick.style.display = "none";
-
   const email = document.querySelector(".email").value;
-  msgSuccess.style.visibility = "visible";
+  successMsg.classList.remove("opacity");
+  failMsg.classList.remove("opacity");
 
   if (isValidEmail(email)) {
-    msgSuccess.classList.add("success");
-    msgSuccess.textContent = "Thank you for subscribing to us.";
-    successTick.style.display = "block";
+    successMsg.classList.add("opacity");
   } else {
-    msgSuccess.classList.add("fail");
-    msgSuccess.textContent = "Sorry! We couldnt find your email.";
-    failTick.style.display = "block";
+    failMsg.classList.add("opacity");
+  }
+
+  if (successMsg.classList.contains("opacity")) {
+    setTimeout(() => {
+      successMsg.classList.remove("opacity");
+    }, 3000);
+
+    console.log("hey");
   }
 });
 
@@ -78,8 +81,30 @@ imgPlay.addEventListener("click", () => {
   document.body.classList.add("no-scroll");
 });
 
+play.addEventListener("click", () => {
+  videoPlayer.classList.add("show");
+  bgBlack.classList.add("show");
+  document.body.classList.add("no-scroll");
+});
+
 crossBlack.addEventListener("click", () => {
   videoPlayer.classList.remove("show");
   bgBlack.classList.remove("show");
   document.body.classList.remove("no-scroll");
+  iframe.src = iframe.src;
+});
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 600) {
+    scroll.classList.add("show");
+  } else {
+    scroll.classList.remove("show");
+  }
+
+  scroll.addEventListener("click", () => {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
